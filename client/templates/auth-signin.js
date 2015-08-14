0c3fb105cfbd2321516009751184e436f45e8732
Steeve <Steeve.fong@pharmagest.mu>
2015-08-14 11:29:19 +0400

    first commit

---
+++ client/templates/auth-signin.js
@@ -0,0 +1,46 @@
+var ERRORS_KEY = 'signinErrors';
+
+Template.signin.onCreated(function() {
+  Session.set(ERRORS_KEY, {});
+});
+
+Template.signin.helpers({
+  errorMessages: function() {
+    return _.values(Session.get(ERRORS_KEY));
+  },
+  errorClass: function(key) {
+    return Session.get(ERRORS_KEY)[key] && 'error';
+  }
+});
+
+Template.signin.events({
+  'submit': function(event, template) {
+    event.preventDefault();
+    
+    var email = template.$('[name=email]').val();
+    var password = template.$('[name=password]').val();
+    
+    var errors = {};
+
+    if (! email) {
+      errors.email = 'Email est obligatoire';
+    }
+
+    if (! password) {
+      errors.password = 'Mot de passe obligatoire';
+    }
+    
+    Session.set(ERRORS_KEY, errors);
+    if (_.keys(errors).length) {
+      return;
+    }
+    
+    Meteor.loginWithPassword(email, password, function(error) {
+      if (error) {
+        return Session.set(ERRORS_KEY, {'none': error.reason});
+      }
+      
+      Router.go('home');
+    });
+  }
+});
---
+++ mobile-config.js
@@ -0,0 +1,35 @@
+App.info({
+  name: 'Marque Verte',
+  description: 'Les services Marque Verte',
+  author: 'Pharmagest Interactive',
+  email: 'steeve.fong@pharmagest.mu',
+  website: 'http://www.marqueverte.com',
+  version: '0.0.1'
+});
+
+App.icons({
+  // iOS
+  'iphone': 'resources/icons/icon-72x72.png',
+  'iphone_2x': 'resources/icons/icon-144x144.png',
+  'ipad': 'resources/icons/icon-72x72.png',
+  'ipad_2x': 'resources/icons/icon-144x144.png',
+
+  // Android
+  'android_ldpi': 'resources/icons/icon-29x29.png',
+  'android_mdpi': 'resources/icons/icon-58x58.png',
+  'android_hdpi': 'resources/icons/icon-72x72.png',
+  'android_xhdpi': 'resources/icons/icon-100x100.png'
+});
+
+App.launchScreens({
+  // iOS
+  'iphone': 'resources/splash/splash-320x480.png',
+  //'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+  'iphone5': 'resources/splash/splash-320x568h@2x.png',
+  // Android
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png'
+});
+
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#000000');
+
